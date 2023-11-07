f07bbe331675e4653f7c4a97f0e1f9ff4a1b0e68
srinidhi1999615 <sreeshant50@gmail.com>
2023-11-07 16:25:34 +0530

    just add it

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://Srinidhi:joshi123@cluster0.wrfovib.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://Srinidhi:joshi123@cluster0.wrfovib.mongodb.net/srinidhibackendtest1dev?retryWrites=true&w=majority');
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,"Error Connecting to the database"));
 db.once('open',function(){
