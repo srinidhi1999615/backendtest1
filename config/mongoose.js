5c6abee364c13c7a72310ca55fe2493b27264680
srinidhi1999615 <sreeshant50@gmail.com>
2023-11-07 15:19:06 +0530

    try again

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/srinidhibackendtest1dev');
+mongoose.connect('mongodb://localhost/srinidhibackendtest1dev');
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,"Error Connecting to the database"));
 db.once('open',function(){
